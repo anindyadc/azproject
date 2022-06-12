595d1e442402d738c70cb7fe3f5a8ef9dc5598fd
anindyadc <anindyadc@gmail.com>
2022-06-12 13:18:39 +0530

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -3,6 +3,6 @@ sudo ufw allow 5000/tcp
 sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
-pip3 install azure.storage
-pip3 install configparser
-pip3 install flask
+sudo pip3 install azure.storage
+sudo pip3 install configparser
+sudo pip3 install flask
